587b8ce6304642626ab3d33e366b5b8a50b31445
pd-Shah <pd.shahsafi@gmail.com>
2017-12-22 17:44:49 +0330

    add models, templates and views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from . import views
+app_name='catalog'
 
 urlpatterns = [
-
+    url(r'^$', views.index, name='index'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_books=models.Book.objects.all().count()
+    num_instances=models.BookInstance.objects.all().count()
+    # Available books (status = 'a')
+    num_instances_available=models.BookInstance.objects.filter(status__exact='a').count()
+    num_authors=models.Author.objects.count()  # The 'all()' is implied by default.
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'catalog/index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+    )
