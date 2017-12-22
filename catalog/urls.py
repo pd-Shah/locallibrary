3822dd74a1ebea4fcb346a8fa636f8d2edf840bf
pd-Shah <pd.shahsafi@gmail.com>
2017-12-22 19:16:20 +0330

    5-list, detail and template view

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,7 @@ from . import views
 app_name='catalog'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render
+from django.views import generic
+
 from . import models
 
-def index(request):
-    """
-    View function for home page of site.
-    """
-    # Generate counts of some of the main objects
-    num_books=models.Book.objects.all().count()
-    num_instances=models.BookInstance.objects.all().count()
-    # Available books (status = 'a')
-    num_instances_available=models.BookInstance.objects.filter(status__exact='a').count()
-    num_authors=models.Author.objects.count()  # The 'all()' is implied by default.
 
-    # Render the HTML template index.html with the data in the context variable
-    return render(
-        request,
-        'catalog/index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
-    )
+class Index(generic.TemplateView):
+
+    template_name='catalog/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['num_books'] = models.Book.objects.all().count()
+        context['num_instances'] = models.BookInstance.objects.all().count()
+        context['num_instances_available'] = models.BookInstance.objects.filter(status__exact='a').count()
+        context['num_authors'] =models.Author.objects.count()
+        return context
+
+class BooKListView(generic.ListView):
+    model=models.Book
+    template_name='catalog/book_list.html'
+
+class BookDetailView(generic.DetailView):
+    model = models.Book
+    template_name='catalog/book_detail.html'
+
+
+# def index(request):
+#     """
+#     View function for home page of site.
+#     """
+#     # Generate counts of some of the main objects
+#     num_books=models.Book.objects.all().count()
+#     num_instances=models.BookInstance.objects.all().count()
+#     # Available books (status = 'a')
+#     num_instances_available=models.BookInstance.objects.filter(status__exact='a').count()
+#     num_authors=models.Author.objects.count()  # The 'all()' is implied by default.
+#
+#     # Render the HTML template index.html with the data in the context variable
+#     return render(
+#         request,
+#         'catalog/index.html',
+#         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+#     )
